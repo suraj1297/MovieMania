132eb1a2ce3f9189641b9cfb53ba5452eb29dba3
surajdesai1297@gmail.com <suraj@LAPTOP-C83DOH9J.localdomain>
2020-10-03 20:46:02 +0530

    Added nikhil's code

--- javascript/app.js
+++ javascript/app.js
@@ -3,4 +3,23 @@ import {
 } from "./getMovies.js"
 
 
-setTimeout(() => getMovies("popular", "en"), 5000)
+getMovies("popular", "en")
+
+let $dropdown = document.querySelector(".dropdown")
+let $language = document.querySelector(".language")
+
+$dropdown.addEventListener("mouseover", () => {
+    $language.style.display = ($dropdown.classList[1] == "fa-caret-down") ? "block" : "none"
+    $dropdown.classList = ($dropdown.classList[1] == "fa-caret-down") ? "fa fa-caret-up dropdown" : "fa fa-caret-down dropdown"
+
+})
+
+
+// document.querySelector(".eng").addEventListener("click", () => 
+// document.querySelector(".telugu").addEventListener("click", () => 
+// document.querySelector(".tamil").addEventListener("click", () => 
+// document.querySelector(".kannada").addEventListener("click", () =>
+// document.querySelector(".malayam").addEventListener("click", () => 
+// document.querySelector(".home").addEventListener("click", () => 
+// document.querySelector(".popular").addEventListener("click", () => 
+// document.querySelector(".upcoming").addEventListener("click", () => 
